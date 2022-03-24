5f39c481042df8867d9ccff99397d5fffe5b3691
Kaustubh Vats <kaustubhvats08@gmail.com>
2022-03-24 20:25:34 +0530

    first commit

---
+++ ir-overview/base.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
+    <link rel="stylesheet" href="/int220/css/root.css"/>
+    <link rel="stylesheet" href="/int220/css/nflx-header.css"/>
+    <link rel="stylesheet" href="/int220/css/hero-nflx.css"/>
+    <link rel="stylesheet" href="/int220/css/nflx-inv-main.css"/>
+    <link rel="stylesheet" href="/int220/css/ir-bottom.css"/>
+    <link rel="stylesheet" href="/int220/css/nflx-footer.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Netflix - Overview - Profile</title>
+</head>
+<body>
+<!-- class="dark-theme" -->
+</body>
+</html>
